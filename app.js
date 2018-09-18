809b98118258a1bde8f4fae2e978228053df9c3c
Tanner Seramur <seramurt@gmail.com>
2018-09-17 22:09:31 -0700

    working on nav

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var shop1 = {
         for(i = 0; i < hours.length; i++){
             var element = document.getElementById('list-1');
             var listItem = document.createElement('li');
-            listItem.textContent =hours[i] + ' ' + shop1.hourlySales[i];
+            listItem.textContent =hours[i] + ': ' + shop1.hourlySales[i];
             element.appendChild(listItem);
         }
     }
@@ -77,7 +77,7 @@ var shop2 = {
         for(i = 0; i < hours.length; i++){
             var element = document.getElementById('list-2');
             var listItem = document.createElement('li');
-            listItem.textContent =hours[i] + ' ' + shop2.hourlySales[i];
+            listItem.textContent =hours[i] + ': ' + shop2.hourlySales[i];
             element.appendChild(listItem);
         }
     }
@@ -110,30 +110,109 @@ var shop3 = {
         var element = document.getElementById('store-title3');
         console.log(element);
         var storeTitle = document.createElement('h3');
-        storeTitle.textContent = shop2.name; 
+        storeTitle.textContent = shop3.name; 
         element.appendChild(storeTitle); 
 
         for(i = 0; i < hours.length; i++){
             var element = document.getElementById('list-3');
             var listItem = document.createElement('li');
-            listItem.textContent =hours[i] + ' ' + shop2.hourlySales[i];
+            listItem.textContent =hours[i] + ': ' + shop3.hourlySales[i];
             element.appendChild(listItem);
         }
     }
      
 }
 
+// shop4
+var shop4 = {
+    name: "Capital Hill",
+    minHourlyCustomers: 20,
+    maxHourlyCustomers: 38,
+    acpc: 2.3,
+    hourlySales: [],
+    totalSale: 0,
+
+    randNum: function(){
+        return Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers;
+    },
+
+    
+    getHourlySales: function(){
+        for(i=0; i < hours.length; i++){
+            var hourSale = Math.floor(this.randNum() * this.acpc);
+            this.hourlySales.push(hourSale);
+            this.totalSale += hourSale;
+
+        }
+    },
+    render: function(){
+        var element = document.getElementById('store-title4');
+        console.log(element);
+        var storeTitle = document.createElement('h3');
+        storeTitle.textContent = shop4.name; 
+        element.appendChild(storeTitle); 
+
+        for(i = 0; i < hours.length; i++){
+            var element = document.getElementById('list-4');
+            var listItem = document.createElement('li');
+            listItem.textContent =hours[i] + ': ' + shop4.hourlySales[i];
+            element.appendChild(listItem);
+        }
+    }
+     
+}
+
+// shop5
+var shop5 = {
+    name: "Alki",
+    minHourlyCustomers: 2,
+    maxHourlyCustomers: 16,
+    acpc: 4.6,
+    hourlySales: [],
+    totalSale: 0,
+
+    randNum: function(){
+        return Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers;
+    },
+
+    
+    getHourlySales: function(){
+        for(i=0; i < hours.length; i++){
+            var hourSale = Math.floor(this.randNum() * this.acpc);
+            this.hourlySales.push(hourSale);
+            this.totalSale += hourSale;
+
+        }
+    },
+    render: function(){
+        var element = document.getElementById('store-title5');
+        console.log(element);
+        var storeTitle = document.createElement('h3');
+        storeTitle.textContent = shop5.name; 
+        element.appendChild(storeTitle); 
+
+        for(i = 0; i < hours.length; i++){
+            var element = document.getElementById('list-5');
+            var listItem = document.createElement('li');
+            listItem.textContent =hours[i] + ': ' + shop5.hourlySales[i];
+            element.appendChild(listItem);
+        }
+    }
+     
+}
+// function for calling store render info
+function storeSalesInfo(theShop){
+    theShop.randNum();
+    theShop.getHourlySales();
+    theShop.render();
+}
 
 
-
-
-shop1.randNum();
-shop1.getHourlySales();
-shop1.render();
-
-shop2.randNum();
-shop2.getHourlySales();
-shop2.render();
+storeSalesInfo(shop1);
+storeSalesInfo(shop2);
+storeSalesInfo(shop3);
+storeSalesInfo(shop4);
+storeSalesInfo(shop5);
 
 
 
