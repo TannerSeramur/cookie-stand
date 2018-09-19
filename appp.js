1d40db16ffb799536ee236acf9dc2c39dbb34897
Tanner Seramur <seramurt@gmail.com>
2018-09-19 14:42:40 -0700

    day 3 of lab

--- appp.js
+++ appp.js
@@ -1,5 +1,9 @@
 // bussiness hours array
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+// Shop.shops = [];
+
+var shopTable = document.getElementById('storeTable');
+var shopForm = document.getElementById('storeForm');
 
 var Shop = function(name, min, max, acpc){
     this.name = name;
@@ -8,6 +12,7 @@ var Shop = function(name, min, max, acpc){
     this.acpc = acpc;
     this.hourlySales = [];
     Shop.totalSale = 0;
+    Shop.shops.push(this);
     this.randNum = function(){
         return Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers;
     };
@@ -17,7 +22,10 @@ var Shop = function(name, min, max, acpc){
             var hourSale = Math.floor(this.randNum() * this.acpc);
             this.hourlySales.push(hourSale);
             Shop.totalSale += hourSale;
+            Shop.total
             console.log(this.hourlySales);
+        }
+        Shop.totalSum.push(Shop.totalSale);
 
         };
     this.render = function(){
@@ -29,23 +37,21 @@ var Shop = function(name, min, max, acpc){
         storeTitle.textContent = this.name; 
         times.appendChild(storeTitle); 
 
-       
-    
         for(i = 0; i < hours.length; i++){
             var timesTD = document.createElement('td');
             timesTD.textContent = this.hourlySales[i];  
             times.appendChild(timesTD)
-            // console.log(timesTD)
-
          }
+
         timesTD = document.getElementById('salesTable');
         var newText = document.createElement('td');
         newText.textContent = Shop.totalSale;
         times.appendChild(newText);
-    }
+    };
 
 }
-}
+Shop.shops = [];
+Shop.totalSum = [];
 
 var shop1 = new Shop('1st and Pike', 23, 65, 6.3);
 var shop2 = new Shop('SeaTac Airport', 3, 34, 1.4);
@@ -53,17 +59,80 @@ var shop3 = new Shop('Seattle Center', 11, 38, 3.7);
 var shop4 = new Shop('Capital Hill', 20, 38, 2.3);
 var shop5 = new Shop('Alki', 2, 16, 4.6);
 
-
 function storeSalesInfo(theShop){
     theShop.randNum();
     theShop.getHourlySales();
     theShop.render();
 }
+
 storeSalesInfo(shop1);
 storeSalesInfo(shop2);
 storeSalesInfo(shop3);
 storeSalesInfo(shop4);
 storeSalesInfo(shop5);
+
+Shop.prototype.getHourTotals = function(){
+    var companyTotal = 0;
+    var element = document.getElementById('tbody');
+    var times = document.createElement('tr');
+    element.appendChild(times);
+    var totalsTitle = document.createElement('th');
+    totalsTitle.textContent = 'Totals: '; 
+    times.appendChild(totalsTitle);
+
+    for(i = 0; i < hours.length; i++){
+        var hourSum = 0
+        for(j = 0; j < Shop.shops.length; j++){
+            hourSum += Shop.shops[j].hourlySales[i];    
+        }
+        var timesTD = document.createElement('td');
+        timesTD.textContent = hourSum;  
+        times.appendChild(timesTD);        
+    }
+    
+    for(i = 0; i < Shop.shops.length; i++){
+        companyTotal += Shop.totalSum[i];
+    }
+    var timesTD = document.createElement('td');
+    timesTD.textContent = companyTotal;  
+    times.appendChild(timesTD);
+}
+
+
+function renderStores() {
+    for(i = 0; i < Shop.shops; i++) {
+      i.render();
+      console.log(Shop.shops[i]);
+    }
+  }
+
+function addNewStore(event){
+    event.preventDefault();
+    console.log(event.target.name.value);
+
+    var newName = event.target.name.value;
+    var newMin = event.target.min.value;
+    var newMax = event.target.max.value;
+    var newACPC = event.target.acpc.value
+
+    new Shop(newName, newMin, newMax, newACPC);
+
+    storeTable.innerHTML = '';
+    storeSalesInfo(Shop.shops);
+    renderStores();
+
+
+
+}
+storeForm.addEventListener('submit', addNewStore);
+// shop1.render();
+storeSalesInfo();
+renderStores();
+
+
+// console.log(event.target.name.value);
+shop1.getHourTotals(Shop.shops);
+
 
 
 
